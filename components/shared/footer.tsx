a05ac2d85e2eeeea8955b557d24eb3358a50a6ca
AnilPatel7177 <anil@elicit.digital>
2025-01-29 14:43:07 +0530

    slider deisgn changes

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -17,14 +17,14 @@ export const Footer = () =>
                     <div className="container mx-auto px-6 py-4 border-b border-t border-[#FFFFFF] ">
                         <nav className="flex flex-wrap justify-between items-center text-sm">
                             <div className="space-x-6">
-                                <a href="#" className="hover:text-gray-300">Home</a>
-                                <a href="#" className="hover:text-gray-300">About Us</a>
-                                <a href="#" className="hover:text-gray-300">Services</a>
-                                <a href="#" className="hover:text-gray-300">Contact Us</a>
+                                <a href="#" className="hover:text-gray-300 text-[16px]">Home</a>
+                                <a href="#" className="hover:text-gray-300 text-[16px]">About Us</a>
+                                <a href="#" className="hover:text-gray-300 text-[16px]">Services</a>
+                                <a href="#" className="hover:text-gray-300 text-[16px]">Contact Us</a>
                             </div>
                             <div className="space-x-6">
-                                <a href="#" className="hover:text-gray-300">Terms & Conditions</a>
-                                <a href="#" className="hover:text-gray-300">Privacy Policy</a>
+                                <a href="#" className="hover:text-gray-300 text-[16px]">Terms & Conditions</a>
+                                <a href="#" className="hover:text-gray-300 text-[16px]">Privacy Policy</a>
                             </div>
                         </nav>
                     </div>
@@ -33,7 +33,7 @@ export const Footer = () =>
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                             <div>
                                 <h2 className="text-2xl font-bold mb-4 text-[#fff]">Logo</h2>
-                                <p className="text-gray-400 text-sm">
+                                <p className="text-gray-400 text-[14px]">
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
                                     diam diam. Praesent sed nisi felis refordare i belogs. FAR
                                     paragrap mundiving och persisk syfliat. Whips poddradio
@@ -42,7 +42,7 @@ export const Footer = () =>
                             </div>
 
                             <div className=" md:text-left p-0">
-                                <h3 className="text-lg font-semibold mb-4 text-[#fff]">Have you any questions?</h3>
+                                <h3 className="text-[20px] font-semibold mb-4 text-[#fff]">Have you any questions?</h3>
                                 <div className="space-y-2 text-gray-400">
                                     <div className="flex gap-2">
                                         <svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -58,29 +58,29 @@ export const Footer = () =>
                                         </svg>
 
                                         <div>
-                                            <p>+44 123 456 7890</p>
-                                            <p>example@email.com</p>
+                                            <p className="text-[16px] text-[#fff]">+44 123 456 7890</p>
+                                            <p className="text-[16px] text-[#fff]">example@email.com</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div className=" w-[130px] ml-auto">
-                                <h3 className="text-lg font-semibold mb-4 text-[#fff]">Follow us on</h3>
+                                <h3 className="text-[20px] font-semibold mb-4 text-[#fff]">Follow us on</h3>
                                 <div className="space-y-2">
-                                    <a href="#" className="flex gap-[23px] items-center hover:text-gray-300"><svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <a href="#" className="text-[16px] flex gap-[23px] items-center hover:text-gray-300"><svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M11.0066 0.00182724C16.5641 -0.104088 21.9974 4.40422 22.002 10.994C22.0066 17.1417 17.1345 22.0046 10.9974 22C4.40934 21.9908 -0.108434 16.5062 0.00197996 10.7822C0.121595 4.87394 5.05802 -0.0902731 11.0066 0.00182724ZM9.29055 19.0666C10.2889 19.0666 11.2596 19.0666 12.2533 19.0666C12.2533 17.8325 12.2533 16.6075 12.2533 15.3826C12.2533 14.1623 12.2533 12.9419 12.2533 11.694C13.2194 11.694 14.1488 11.694 15.0873 11.694C15.0873 10.7315 15.0873 9.79209 15.0873 8.82044C14.1396 8.82044 13.2056 8.82044 12.2947 8.82044C12.2947 8.03758 12.2671 7.28696 12.3039 6.54095C12.3269 6.05743 12.626 5.7581 13.1044 5.66139C13.3667 5.61074 13.6335 5.59692 13.9003 5.58771C14.3282 5.5739 14.7514 5.58311 15.1931 5.58311C15.1931 4.68052 15.1931 3.80097 15.1931 2.93983C14.2546 2.93983 13.3253 2.87996 12.4097 2.95364C10.7029 3.0964 9.52518 4.26607 9.35956 5.96993C9.27675 6.82186 9.31355 7.683 9.29975 8.53953C9.29975 8.62702 9.29975 8.71912 9.29975 8.82965C8.45785 8.82965 7.64814 8.82965 6.83384 8.82965C6.83384 9.79209 6.83384 10.7269 6.83384 11.7032C7.66195 11.7032 8.47625 11.7032 9.29975 11.7032C9.29055 14.1715 9.29055 16.6075 9.29055 19.0666Z" fill="#DDDDDD" />
                                     </svg>
                                         Facebook</a>
-                                    <a href="#" className="flex gap-[23px] items-center hover:text-gray-300"><svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <a href="#" className="text-[16px] flex gap-[23px] items-center hover:text-gray-300"><svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M3.22183 3.22183C5.28473 1.15893 8.08262 0 11 0C13.9174 0 16.7153 1.15893 18.7782 3.22183C20.8411 5.28473 22 8.08262 22 11C22 13.9174 20.8411 16.7153 18.7782 18.7782C16.7153 20.8411 13.9174 22 11 22C8.08262 22 5.28473 20.8411 3.22183 18.7782C1.15893 16.7153 0 13.9174 0 11C0 8.08262 1.15893 5.28473 3.22183 3.22183ZM14.8713 7H13.4593L11.132 9.66603L9.12228 7.00066H6.20898L9.6885 11.5626L6.38983 15.344H7.80308L10.3481 12.4274L12.5748 15.344H15.4158L11.787 10.5341L14.8713 7ZM13.7454 14.497H12.9628L7.85569 7.80296H8.69482L13.7454 14.497Z" fill="#DDDDDD" />
                                     </svg>
                                         Twitter</a>
-                                    <a href="#" className="flex gap-[23px] items-center hover:text-gray-300"><svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <a href="#" className="text-[16px] flex gap-[23px] items-center hover:text-gray-300"><svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M10.8227 0.00183718C16.9146 -0.108589 21.9298 4.77317 22.0034 10.8144C22.077 16.9707 17.209 21.9491 11.1034 21.9997C5.06214 22.0457 0.0791465 17.1639 0.000927868 11.1871C-0.0772908 5.07225 4.79987 0.0984602 10.8227 0.00183718ZM9.30895 8.58288C9.30895 11.2699 9.30895 13.9432 9.30895 16.6164C10.2016 16.6164 11.0804 16.6164 11.973 16.6164C11.973 16.2023 11.973 15.8066 11.973 15.4109C11.9776 14.233 11.973 13.0551 11.9914 11.8773C11.996 11.3895 12.2537 11.0169 12.6401 10.7454C13.4131 10.1979 14.4208 10.5429 14.6784 11.4494C14.7566 11.7162 14.7889 12.0061 14.7935 12.2868C14.8027 13.6487 14.7981 15.0106 14.7981 16.3725C14.7981 16.4554 14.7981 16.5336 14.7981 16.621C15.7045 16.621 16.5787 16.621 17.4621 16.621C17.4667 16.5704 17.4713 16.5336 17.4713 16.4968C17.4713 14.9186 17.4851 13.345 17.4575 11.7668C17.4483 11.3205 17.3701 10.865 17.2412 10.4371C16.9008 9.30985 16.1416 8.61049 14.9683 8.43104C13.7904 8.2516 12.7782 8.54147 12.0696 9.57212C12.0558 9.59512 12.0282 9.60893 11.9638 9.66874C11.9638 9.27765 11.9638 8.92796 11.9638 8.58748C11.0574 8.58288 10.1878 8.58288 9.30895 8.58288ZM5.14956 8.58288C5.14956 11.2699 5.14956 13.9432 5.14956 16.6118C6.04217 16.6118 6.92098 16.6118 7.79979 16.6118C7.79979 13.9294 7.79979 11.2607 7.79979 8.58288C6.91178 8.58288 6.03297 8.58288 5.14956 8.58288ZM6.45627 7.47402C7.30747 7.48322 7.91942 6.9633 7.98383 6.17651C8.04825 5.43573 7.53293 4.82379 6.74154 4.70416C5.93174 4.57993 5.19097 5.01703 5.01612 5.721C4.78607 6.65502 5.44863 7.46481 6.45627 7.47402Z" fill="#DDDDDD" />
                                     </svg>
                                         LinkedIn</a>
-                                    <a href="#" className="flex gap-[23px] items-center hover:text-gray-300"><svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <a href="#" className="text-[16px] flex gap-[23px] items-center hover:text-gray-300"><svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M10.8145 0.00183211C16.9207 -0.108594 21.9364 4.78696 22.0054 10.8282C22.0745 16.9614 17.2106 21.9444 11.132 21.9996C5.06253 22.0502 0.0744482 17.1777 0.000823433 11.1733C-0.0728014 5.06763 4.80024 0.103056 10.8145 0.00183211ZM11.0077 17.3525C11.0077 17.3249 11.0077 17.3019 11.0077 17.2743C11.9096 17.2743 12.8161 17.2881 13.718 17.2697C14.3577 17.2559 14.9789 17.1179 15.5449 16.8004C16.709 16.1378 17.2336 15.0796 17.2658 13.7959C17.3119 11.937 17.298 10.0782 17.2704 8.21937C17.2566 7.32676 16.9759 6.50776 16.3501 5.8314C15.6507 5.08143 14.7626 4.75935 13.7779 4.74555C11.9326 4.71334 10.0874 4.71334 8.2422 4.74555C7.29888 4.75935 6.43839 5.06302 5.74816 5.75319C5.04412 6.45715 4.76803 7.33596 4.74042 8.29298C4.70361 9.56288 4.6944 10.8328 4.699 12.1027C4.699 12.7836 4.71741 13.4646 4.78183 14.141C4.95209 15.8618 6.14849 17.0718 7.86487 17.2237C8.90942 17.3157 9.95858 17.3111 11.0077 17.3525Z" fill="#DDDDDD" />
                                         <path d="M11.007 5.80273C11.9549 5.83034 12.9028 5.84414 13.8508 5.88555C14.3523 5.90856 14.8309 6.04659 15.2404 6.35946C15.8202 6.79657 16.1009 7.41311 16.1101 8.10787C16.1377 10.0357 16.1607 11.9636 16.1055 13.8868C16.0687 15.1981 15.1852 16.0493 13.8692 16.1045C12.4105 16.1644 10.9472 16.1552 9.48389 16.1598C8.95932 16.1598 8.42554 16.1506 7.90556 16.0723C6.68155 15.8929 5.9269 15.1153 5.89469 13.8822C5.84407 11.9544 5.83947 10.0265 5.89469 8.10327C5.9315 6.79197 6.815 5.97758 8.13104 5.87635C8.3197 5.86255 8.51297 5.83954 8.70163 5.83954C9.46549 5.83494 10.2339 5.83954 10.9978 5.83954C11.007 5.83034 11.007 5.81654 11.007 5.80273ZM11.0162 14.2411C12.8108 14.2365 14.2511 12.7872 14.2511 10.9927C14.2465 9.19833 12.797 7.75819 11.0024 7.76279C9.2078 7.76739 7.76292 9.21673 7.76752 11.0112C7.76752 12.801 9.22161 14.2457 11.0162 14.2411ZM14.3891 6.87479C13.9658 6.87018 13.6253 7.20146 13.6207 7.62016C13.6161 8.03886 13.9566 8.38394 14.3753 8.38394C14.7849 8.38394 15.1208 8.05726 15.13 7.64317C15.1392 7.21527 14.8171 6.87939 14.3891 6.87479Z" fill="#DDDDDD" />
                                         <path d="M11.0259 8.89943C12.1901 8.90403 13.115 9.84725 13.1104 11.0159C13.1058 12.18 12.1625 13.1048 10.9937 13.1002C9.82951 13.0956 8.9046 12.1524 8.9092 10.9837C8.9138 9.81964 9.85251 8.89483 11.0259 8.89943Z" fill="#DDDDDD" />
@@ -93,7 +93,7 @@ export const Footer = () =>
                     <div className="container ">
 
                     <div className=" border-t border-[#FFFFFF] mt-12">
-                        <div className="container mx-auto px-6 py-4 text-center text-sm text-gray-400">
+                        <div className="container mx-auto px-6 py-4 text-center text-[14px] text-gray-400">
                             Copyright © 2022 works . All rights reserved.
                         </div>
                     </div>
