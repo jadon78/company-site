32435b218468a85c1eb8941f63b1a083fe5cfa71
developerfullstack12 <developerfullstack12@gmail.com>
2025-02-10 21:08:20 +0530

    module chsnges

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -225,7 +225,7 @@ const ContactUs = () => {
                     </div>
                 </div>
             </div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.4805698539294!2d75.89368257592147!3d22.747540126534744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fd0267024913%3A0x9a995d6abcd51279!2sKrozzon!5e0!3m2!1sen!2sin!4v1739166184433!5m2!1sen!2sin" width="100%" height="450" style={{border:"0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.4805698539294!2d75.89368257592147!3d22.747540126534744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fd0267024913%3A0x9a995d6abcd51279!2sKrozzon!5e0!3m2!1sen!2sin!4v1739166184433!5m2!1sen!2sin" width="100%" height="450" style={{border:"0"}} loading="lazy"></iframe>
         </div>
     </>)
 }
